2645a60c25bf2c8a9d410b7f81ccf990e2094a26
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2018-02-08 22:23:51 +0900

    init

---
+++ sample-scripts/basicSample.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+    // 変数代入
+    msg := "hello world"
+    fmt.Println(msg)
+
+    num := 12345
+    fmt.Println(num)
+
+
+    // 定数
+    const sampleConst = "sample message"
+    fmt.Println(sampleConst)
+}
