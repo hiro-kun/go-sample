e611a224b776d757a84879fc8fbdbe90a2b42999
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2018-02-11 15:00:33 +0900

    add 関数サンプルを追加

--- sample-scripts/basicSample.go
+++ sample-scripts/basicSample.go
@@ -6,12 +6,25 @@ func main() {
     // 変数代入
     msg := "hello world"
     fmt.Println(msg)
+    // hello world
 
     num := 12345
     fmt.Println(num)
+    // 12345
 
 
     // 定数
     const sampleConst = "sample message"
     fmt.Println(sampleConst)
+    // sample message
+
+
+    // 関数
+    fmt.Println(sampleFunc("yamada-tarou"))
+    // Input name is yamada-tarou
+}
+
+func sampleFunc(name string) (/* ここに帰り値を記述する事によりreturnの後にreturnする変数名を記述しなくていい */ msg string) {
+    msg = "Input name is " + name
+    return
 }
