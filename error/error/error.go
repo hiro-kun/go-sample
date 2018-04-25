de4ebd19a819fac2594bdd3b1a63bf4fcfe1ffd0
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2018-04-25 22:31:29 +0900

    fix エラー・メッセージの先頭を小文字に修正

--- error/error/error.go
+++ error/error/error.go
@@ -9,7 +9,7 @@ func Sample() error {
 	// 存在しないファイルを読み込む
 	_, err := os.Open("./sample.txt")
 	if err != nil {
-		return fmt.Errorf("File open failed. Error message is : %s", err)
+		return fmt.Errorf("file open failed. Error message is : %s", err)
 	}
 
 	return nil
