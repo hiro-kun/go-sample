04c9c90aa63e7a62a132976969b0e4963cf6eb4c
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2018-05-14 01:29:00 +0900

    fix 並列実行可能なように修正

--- imageresize/cmd/resize.go
+++ imageresize/cmd/resize.go
@@ -6,6 +6,8 @@ import (
     "fmt"
     "os"
     "io/ioutil"
+    "sync"
+    "strings"
 
     "github.com/disintegration/imaging"
     "github.com/spf13/cobra"
@@ -22,9 +24,6 @@ var reizeCmd = &cobra.Command{
     Long:  "resize cmd.",
     Run: func(cmd *cobra.Command, args []string) {
 
-        // CPUコア数に応じてチャンネル数の上限を決める
-        finished := make(chan bool)
-
         baseDir := os.Getenv("GOPATH") + "/src" + conf.BaseImageDir
         // outDir := os.Getenv("GOPATH") + "/src" + conf.OutImageDir
 
@@ -36,34 +35,24 @@ var reizeCmd = &cobra.Command{
         var fileList[]string
         for _, file := range files {
           fileName := baseDir + file.Name()
+
+          // 一時的に.gitkeepを外す。根本解決は今度
+          if strings.Contains(fileName, ".gitkeep") {
+            continue
+          }
+
           fileList = append(fileList, fileName)
-          fmt.Println()
         }
 
-        go func() {
-          // load an image from file
-          srcImage, err := imaging.Open(fileList[1])
-          if err != nil {
-            panic(err)
-          }
+        d := NewWorker()
 
-          // save the image to file
-          err = imaging.Save(srcImage, "/tmp/test.png")
-          if err != nil {
-            panic(err)
-          }
-        } ()
+        for _, file := range fileList {
+          d.Add(file)
+        }
 
-        // fmt.Println(args)
-        // fmt.Println(baseDir)
-        // fmt.Println(outDir)
-        // fmt.Println(fileList)
-        // fmt.Println(fileList[1])
+        d.Start()
 
-        /*
-           一つずつ取り出してresizeしていく。そこでgoルーチンを利用
-           resize開始と終了でCUI上にデバッグ処理を出す
-        */
+        d.Stop()
 
         /* プログラムの設計
              ※拡張子は指定しない。
@@ -72,4 +61,69 @@ var reizeCmd = &cobra.Command{
            - go run main.go resize height width
         */
     },
+}
+
+
+type (
+	// Worker 構造体定義
+	Worker struct {
+		queue chan interface{}
+		wg    sync.WaitGroup
+	}
+)
+
+const (
+	maxWorkers = 2
+	queueLimit = 1000
+)
+
+// NewWorker NewWorker生成
+func NewWorker() *Worker {
+	d := &Worker{
+		queue: make(chan interface{}, queueLimit),
+	}
+	return d
+}
+
+// Add キューに処理を追加
+func (d *Worker) Add(v interface{}) {
+	d.queue <- v
+}
+
+// Start Worker開始
+func (d *Worker) Start() {
+	d.wg.Add(maxWorkers)
+	for i := 0; i < maxWorkers; i++ {
+		go func() {
+			defer d.wg.Done()
+			for v := range d.queue {
+				if str, ok := v.(string); ok {
+					resize(str)
+				}
+			}
+		}()
+	}
+}
+
+// Stop 停止処理
+func (d *Worker) Stop() {
+	close(d.queue)
+	d.wg.Wait()
+}
+
+// get API call ダミー
+func resize(filePath string) {
+fmt.Println(filePath)
+
+  // load an image from file
+  srcImage, err := imaging.Open(filePath)
+  if err != nil {
+    panic(err)
+  }
+
+  // save the image to file
+  err = imaging.Save(srcImage, "/tmp/test.png")
+  if err != nil {
+    panic(err)
+  }
 }
