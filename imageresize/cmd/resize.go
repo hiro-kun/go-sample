7798751b41e80113058c9950fc1f0031084a5b2f
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2018-05-24 02:08:24 +0900

    fix 入力値のバリエーションチェックを追加

--- imageresize/cmd/resize.go
+++ imageresize/cmd/resize.go
@@ -7,11 +7,10 @@ import (
     "fmt"
     "io/ioutil"
     "strings"
-    "strconv"
 
     "github.com/spf13/cobra"
     "github.com/go-ozzo/ozzo-validation"
-    
+
     "imageresize/imageresize/lib/resize"
     "imageresize/imageresize/lib/conf"
 )
@@ -20,43 +19,62 @@ func init() {
     RootCmd.AddCommand(reizeCmd)
 }
 
+type opt struct {
+  width int
+  height int
+}
+
+func (o opt) Validate() error {
+	return validation.ValidateStruct(&o,
+    // TODO エラー・メッセージを定数化
+    validation.Field(&o.height, validation.Required, validation.Min(10).Error("error. height input between 10 to 500"), validation.Max(500).Error("error. height input between 10 to 500")),
+    validation.Field(&o.height, validation.Required, validation.Min(10).Error("error. height input between 10 to 500"), validation.Max(500).Error("error. height input between 10 to 500")),
+	)
+}
+
 var reizeCmd = &cobra.Command{
     Use:   "resize",
     Long:  "resize cmd.",
     Run: func(cmd *cobra.Command, args []string) {
+      var opt = &opt{}
 
-        width, _ := strconv.Atoi(args[0])
-        height, _ := strconv.Atoi(args[1])
+      opt.width, _ = strconv.Atoi(args[0])
+      opt.height, _ = strconv.Atoi(args[1])
 
-        baseDir := os.Getenv("GOPATH") + conf.BaseImageDir
+      if err := opt.Validate(); err != nil {
+		      fmt.Println(err)
+		      os.Exit(1)
+	    }
 
-        files, err := ioutil.ReadDir(baseDir)
-        if err != nil {
-          fmt.Println("directory open error. %s", err)
-          os.Exit(1)
+      baseDir := os.Getenv("GOPATH") + conf.BaseImageDir
+
+      files, err := ioutil.ReadDir(baseDir)
+      if err != nil {
+        fmt.Println("directory open error. %s", err)
+        os.Exit(1)
+      }
+
+      var fileList[]string
+      for _, file := range files {
+        fileName := baseDir + file.Name()
+
+        // TODO 一時的に.gitkeepを外す。根本解決は今度
+        // 拡張子を特定の拡張子以外は弾くようにする
+        if strings.Contains(fileName, ".gitkeep") {
+          continue
         }
 
-        var fileList[]string
-        for _, file := range files {
-          fileName := baseDir + file.Name()
+        fileList = append(fileList, fileName)
+      }
 
-          // TODO 一時的に.gitkeepを外す。根本解決は今度
-          // 拡張子を特定の拡張子以外は弾くようにする
-          if strings.Contains(fileName, ".gitkeep") {
-            continue
-          }
+      w := resize.NewWorker()
+      for _, file := range fileList {
+        w.Add(file)
+      }
 
-          fileList = append(fileList, fileName)
-        }
+      w.Start(opt.width, opt.height)
 
-        w := resize.NewWorker()
-        for _, file := range fileList {
-          w.Add(file)
-        }
-
-        w.Start(width, height)
-
-        w.Stop()
+      w.Stop()
 
         /* プログラムの設計
              ※拡張子は指定しない。
